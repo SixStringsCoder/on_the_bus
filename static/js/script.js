f4c01deb178dc881d53d700f2fd64050d967ee3f
Steve Hanlon <rshanlon@gmail.com>
2017-06-07 18:28:46 -0700

    add Comment Tags for bus stops, wire up jQuery slider to control search radius, add 2nd ajax call for Arrival times

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,16 @@
 /*
- Created on.
+ Created by Steve Hanlon on June 5, 2017.
 */
 
 
 // Declare Closure Variables
 let map;
+let stopMarkers = [];
+
 
 
 function addStopToTable(index, bus){
-    //Make some table html in jQuery Here
+    //Make some HTML table with jQuery
 
     // <tr>
     //   <th scope="row">1</th>
@@ -27,145 +29,221 @@ function addStopToTable(index, bus){
 }
 
 
-function addBusStopMarker(bus) {
-
-    let busStopLoc = new google.maps.LatLng(bus.lat, bus.lng);
-
+function addMarker(busStop) {
+    // Make Info Windows
 
 
-    let stopMarker = new google.maps.Marker({
-        position: bus,
-        title: bus.desc,
-        map: map,
+    let contentString = makeInfoWindow(busStop);
+
+    let infowindow = new google.maps.InfoWindow({
+        content: contentString
     });
 
-    // To add the marker to the map, call setMap();
-    stopMarker.setMap(map);
+    let iconPath = 'static/images/tourist-bus-icon_lg.png';     //Change default Google icon
+
+    let stopMarker = new google.maps.Marker({
+        position: busStop,
+        map: map,
+        icon: iconPath
+    });
+
+    stopMarker.setMap(map);                                 //Adds a single bus stop to the map
+
+    stopMarker.addListener('click', function () {
+        infowindow.open(map, stopMarker);
+    });
+
+    stopMarkers.push(stopMarker);                        // Push this to stopMarker to the markers array
+}  // END addMarkers function
+
+
+
+function makeInfoWindow(busStop) {
+    //Generates and adds info-window HTML and Google Map marker objects
+
+    // desc:"SW Kelly & Corbett"
+    // dir: "Northbound"
+    // lat:45.5019212692922
+    // lng:-122.675276776225
+    // locid:3116
+
+
+    let $headings = $('<h5>', {id: "firstHeading"}, {class: "firstHeading"}).text('Bus: #' + busStop.locid);
+
+    let $bodyContent = $('<div>', {id: "bodyContent"})
+    let $busDetails = $('<p>', {id: "busDetails"}).text('Direction:' + busStop.dir + '<br>' + 'Stop Location:' + busStop.desc);
+
+    let $content = $('<main>').append($headings, $bodyContent);
+    return $content.html();
 }
 
 
-function addMarkers(busses){
-    //Adds a collections of busses to map and table
+function addBusStopMarkers(busStops) {
+    //Adds a collections of buses to map and table
     let iconBase = 'static/images/tourist-bus-icon_lg.png';
 
-    $.each(busses, function(index, bus) {
-        console.log(bus);
+    $.each(busStops, function (index, busStop) {
+        addStopToTable(index, busStop);                     // Add this to stopMarker to the table
+        addMarker(busStop);                                 // Add this to stopMarker to the map
 
-        //Change default Google icon
-        let iconBase = 'static/images/tourist-bus-icon_lg.png';
-
-        //Adds a single bus stop to the map
-        let stopMarker = new google.maps.Marker({
-          position: bus,
-          map: map,
-          icon: iconBase
-        });
-
-        // Make Info Windows
-        let $content = $('<div>', {id: "content"});
-        let $siteNotice = $('<div>', {id: "siteNotice"});
-        let headings = $('<h1>', {id: "firstHeading"}, {class: "firstHeading"});
-        let $bodyContent = $('<div>', {id: "bodyContent"}, '<p>');
-
-        let contentString =
-
-      //   var contentString = '<div id="content">'+
-      // '<div id="siteNotice">'+
-      // '</div>'+
-      // '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      // '<div id="bodyContent">'+
-      // '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      // 'sandstone rock formation in the southern part of the '+
-      // 'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      // 'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      // '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      // 'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      // 'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      // 'Aboriginal people of the area. It has many springs, waterholes, '+
-      // 'rock caves and ancient paintings. Uluru is listed as a World '+
-      // 'Heritage Site.</p>'+
-      // '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      // 'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      // '(last visited June 22, 2009).</p>'+
-      // '</div>'+
-      // '</div>';
-
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString
-        });
-
-      stopMarker.addListener('click', function() {
-      infowindow.open(map, stopMarker);
-      });
-
-    // To add the marker to the map, call setMap();
-    stopMarker.setMap(map);
-
-    addStopToTable(index, bus);
-
-    });}
+        console.log(busStop);
+    });
+}
 
 
-function fetch(here){
-    // AJAX request
+
+function fetchArrivals(locID) {
+    // AJAX REQUEST to Get ARRIVAL TIMES
     let data;
 
-    let lat = here.coords.latitude;
-    let long = here.coords.longitude;
-
     //Object Request
-    let req_params = {'appID': 'DD56C4E4A2D654695936E059E',
-                      'json': 'true',
-                      'll': `${lat}, ${long}`,
-                      'meters': '200'};
+    let arrive_params = {
+        'appID': 'DD56C4E4A2D654695936E059E',
+        'json': 'true',
+        'locIDs': locID,
+        'arrivals': '2'
+    };
 
     $.ajax({
-        url: 'https://developer.trimet.org/ws/V1/stops',
+        url: 'https://developer.trimet.org/ws/V1/arrivals',
         method: 'GET',
-        data: req_params,
-        success: function(response){
+        data: arrive_params,
+        success: function (response) {
             console.log(response);
             data = response;
-            let busses = response.resultSet.location;
-            addMarkers(busses);
+            let busTimes = response.resultSet.arrival;
+            // addBusStopMarkers(busses);
         },
-        error: function(error){
+        error: function (error) {
             console.log(error);
         }
     });
 }
 
 
-// jQuery UI Snap to Increment slider
-$( function() {
-    $( "#slider" ).slider({
-      value: 100,
-      min: 0,
-      max: 1000,
-      step: 50,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value + " meters");
-        // stop: function( event, ui ) {
-        //
-        // }
-      }
-    });
-    $( "#amount" ).val( $( "#slider" ).slider( "value" ) + " meters");
-  } );
 
+
+function fetch(here, meters) {
+    // AJAX REQUEST
+    if ( typeof meters === 'undefined' ) {
+        let meters = '100';
+    }
+    let data;
+
+    let lat = here.coords.latitude;
+    let long = here.coords.longitude;
+
+    //Object Request
+    let req_params = {
+        'appID': 'DD56C4E4A2D654695936E059E',
+        'json': 'true',
+        'll': `${lat}, ${long}`,
+        'meters': meters
+        // 'Arr': arrTime,
+    };
+
+    $.ajax({
+        url: 'https://developer.trimet.org/ws/V1/stops',
+        method: 'GET',
+        data: req_params,
+        success: function (response) {
+            console.log(response);
+            data = response;
+            let busses = response.resultSet.location;
+            addBusStopMarkers(busses);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
+
+
+function setMapOnAll(map) {
+    // Sets the map on all markers in the array.
+    $.each(stopMarkers, function (index, stopMarker) {
+        stopMarker.setMap(map);
+    });
+}
+
+
+function clearMarkers() {
+    // Removes the markers from the map, but keeps them in the array.
+    setMapOnAll(null);
+}
+
+
+function showMarkers() {
+    // Shows any markers currently in the array.
+    setMapOnAll(map);
+}
+
+
+function deleteMarkers() {
+    // Deletes all markers in the array by removing references to them.
+    clearMarkers();
+    stopMarkers = [];
+}
+
+
+function clearMap() {
+    // Clears all the markers from the map
+
+}
+
+function clearTable() {
+    // Clears all the records from the table
+    $("#busses").empty();
+}
+
+
+function updateStops(event, ui) {
+    console.log('Slider just stopped');
+    // Get Geolocation
+    // Get new bus data
+    //To obtain the user's current location, you can call the getCurrentPosition() method.
+    navigator.geolocation.getCurrentPosition(function (position) {
+        fetch(position, ui.value);
+    });
+
+    // Clear the map
+    clearMarkers();
+    // Clear the Table
+    clearTable();
+    // Update the map
+}
+
+
+
+$(function () {
+    // jQuery UI Snap to Increment slider
+    let options = {
+        value: 100, min: 0, max: 1000, step: 100,
+        slide: function (event, ui) {
+            $("#amount").val(ui.value + " meters");
+        },
+        stop: updateStops  // function object
+    };
+
+    $("#slider").slider(options);
+
+    $("#amount").val($("#slider").slider("value") + " meters");
+
+
+});
 
 
 function initMap(position) {
-        let here = {lat: position.coords.latitude, lng: position.coords.longitude};
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 16,
-          center: here
-        });
-        let marker = new google.maps.Marker({
-          position: here,
-          map: map
-        });
-      }
+    let here = {lat: position.coords.latitude, lng: position.coords.longitude};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: here
+    });
+    let marker = new google.maps.Marker({
+        position: here,
+        map: map
+    });
+}
 
 
 function getPosition() {
@@ -177,15 +255,14 @@ function getPosition() {
         //To obtain the user's current location, you can call the getCurrentPosition() method.
         navigator.geolocation.getCurrentPosition(function (position) {
             // console.log(position);
-        // Call Google Init Function for MAP and MARKER
-        initMap(position);
-        fetch(position);
+            // Call Google Init Function for MAP and MARKER
+            initMap(position);
+            fetch(position);
         });
 
-    } else {
-        /* geolocation IS NOT available */
-        console.log("No location available");
-    }
+        } else {
+            /* geolocation IS NOT available */
+            console.log("No location available");
+        }
 }
-
 
